c4fe025b7b774adf702f197f328772dfeaf44788
Alexander Torrijos <artorrijos@gmail.com>
2020-12-26 02:32:30 +0800

    Updates

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello Expos</Text>
       <StatusBar style="auto" />
     </View>
   );
